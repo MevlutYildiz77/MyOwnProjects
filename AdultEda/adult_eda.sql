0ed59dc2398bc02749720a0d23ae450e95e5c082
MevlutYildiz77 <mevlutyildiz957@gmail.com>
2023-01-03 17:50:48 +0300

    sent

--- AdultEda/adult_eda.sql
+++ AdultEda/adult_eda.sql
@@ -7,7 +7,7 @@ SELECT *
 FROM adult_eda
 WHERE workclass IN ('?')
 
---REPLACE '?' WITH 'Unknown' STATEMENT
+--REPLACE '?' WITH 'Unknown' STATEMENT in workclass column
 SELECT REPLACE(workclass, '?', 'Unknown') as workclass
 FROM adult_eda
 
@@ -40,7 +40,7 @@ GROUP BY education
 ORDER BY CountsOfEducationKinds DESC
 
 
---What percentage of people with advanced education (Bachelors, Masters, or Doctorate) make more than 50K? ---
+--??????????What percentage of people with advanced education (Bachelors, Masters, or Doctorate) make more than 50K? ---
 SELECT  COUNT(salary) as salary
 FROM adult_eda
 WHERE education IN('Bachelors', 'Masters', 'Doctorates')
@@ -54,9 +54,40 @@ WHERE salary > '50K'
 GROUP BY education
 ORDER BY salary_order
 
---What country has the highest percentage of people that earn >50K?  --
+--????What country has the highest percentage of people that earn >50K?  --
 SELECT native_country, COUNT(MAX(salary))as MaxSalary
 FROM adult_eda
 WHERE salary = '>50K'
 GROUP BY native_country
 ORDER BY MaxSalary
+
+---Do education and education_num columns hold similar information? 
+SELECT education, education_num, COUNT(education_num) as CountOfEducation
+FROM adult_eda
+GROUP BY education, education_num
+ORDER BY CountOfEducation DESC
+
+-- Impute missing values in relationship column with 'Unmarried'. Also replace 'Not-in-family','Other-relative' values in this column with 'Unmarried'?
+update adult_eda set relationship = replace(relationship, 'NULL', 'Unmaried');
+update adult_eda set relationship = replace(relationship, 'Other-relative', 'Unmarried');
+update adult_eda set relationship = replace(relationship, 'Not-in-family', 'Unmarried');
+
+select relationship, COUNT('NULL') as null_amount
+from adult_eda
+--where relationship = 'NULL'
+GROUP BY relationship
+ORDER BY null_amount
+
+--Identify the most popular occupation for those who earn >50K in India.
+SELECT native_country, occupation, COUNT(occupation) as most_occupation
+FROM adult_eda
+WHERE native_country IN('India')
+AND salary IN('>50K')
+GROUP BY native_country, occupation
+ORDER BY most_occupation DESC
+
+
+
+
+
+
